9d049a55c7e5ea4ec48e39dbc6ec0f09125582fd
Ari Leskinen <arrrtsi@gmail.com>
2020-11-02 20:54:55 +0200

    client_c nimi appi
    Komponenttien apufunktiot sekä destrukturointi tai defaktorointi

--- osa1/client_c/src/index.js
+++ osa1/client_c/src/index.js
@@ -1,17 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Hello = ({ name, age }) => {
+  // Vakiot ja muuttujat
+  // const name = props.name;
+  // const age = props.age;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  // Vakiot ja muuttujat parempi tapa
+  // const { name, age } = props;
+
+  // Syntymävuosi funktio
+  // const bornYear = () => {
+  //   const yearNow = new Date().getFullYear();
+  //   return yearNow - age;
+  // };
+
+  // Syntymävuosi funktio refaktoroituna
+  // const bornYear = () => {
+  //   return new Date().getFullYear() - age;
+  // };
+
+  // Syntymävuosi funktio edelleen refaktoroituna
+  const bornYear = () => new Date().getFullYear() - age;
+
+  return (
+    <div>
+      <p>
+        Hello {name}, you are {age} years old
+      </p>
+      <p>So you were probably born {bornYear()}</p>
+    </div>
+  );
+};
+
+const App = () => {
+  const nimi = 'Pekka';
+  const ika = 10;
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name='Maya' age={26 + 10} />
+      <Hello name={nimi} age={ika} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
