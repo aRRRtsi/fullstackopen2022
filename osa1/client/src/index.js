e791a2e122f479578a1ba876324e59891404fd7f
Ari Leskinen <arrrtsi@gmail.com>
2020-11-04 14:08:48 +0200

    Epätoivoinen yritys
    Epätoivoinen refactorointiyritys, tilanhallinta ei vaa mene mun kaaliin.
    Pitää yrittää opiskella lisää ja palata asiaan myöhemmin.

--- osa1/client/src/index.js
+++ osa1/client/src/index.js
@@ -10,21 +10,39 @@ const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
 );
 
+// Statistics component
+const Statistics = (props) => {
+  const averageSum = ((props.good - props.bad) / props.all).toFixed(2);
+  const positiveSum = ((props.good / props.all) * 100).toFixed(2);
+
+  return (
+    <section>
+      <SecondaryHeading content='Statistics' />
+      <p>Good {props.good}</p>
+      <Stat text='Good' stat={props.good} />
+      <Stat text='Neutral' stat={props.neutral} />
+      <Stat text='Bad' stat={props.bad} />
+      <Stat text='ALL' stat={props.all} />
+      <hr />
+      <Stat text='Average' stat={averageSum} />
+      <Stat text='Positive' stat={positiveSum + ' %'} />
+    </section>
+  );
+};
+
+// Stat component
 const Stat = ({ text, stat }) => (
   <p>
     {text} {stat}
   </p>
 );
 
+// APP component
 const App = () => {
-  // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
   const [all, setAll] = useState(0);
-
-  const averageSum = ((good - bad) / all).toFixed(2);
-  const positiveSum = ((good / all) * 100).toFixed(2);
 
   const handleClickGood = () => {
     setGood(good + 1);
@@ -50,14 +68,7 @@ const App = () => {
       &nbsp;
       <Button handleClick={handleClickBad} text='Bad' />
       &nbsp;
-      <SecondaryHeading content='Statistics' />
-      <Stat text='Good' stat={good} />
-      <Stat text='Neutral' stat={neutral} />
-      <Stat text='Bad' stat={bad} />
-      <Stat text='ALL' stat={all} />
-      <hr />
-      <Stat text='Average' stat={averageSum} />
-      <Stat text='Positive' stat={positiveSum + ' %'} />
+      <Statistics />
     </div>
   );
 };
