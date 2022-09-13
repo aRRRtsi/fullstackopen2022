b457071b85eddc10b9d960182789ecb1a94b4eca
aRRRtsi <arrrtsi@gmail.com>
2022-09-13 18:15:18 +0300

    1.2: kurssitiedot, step2
    Tein sovelluksen step2 työvaiheet

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -19,27 +19,29 @@ const App = () => {
 
 const Header = ({course}) => {
   return (
-  <>
    <h1>{course}</h1>
-  </>
   )
 }
 
 const Content = ({part1, part2, part3, exercises1, exercises2, exercises3}) => {
   return (
-  <>
-   <p>{part1} {exercises1}</p>
-   <p>{part2} {exercises2}</p>
-   <p>{part3} {exercises3}</p>
-  </>
+  <div>
+   <Part part={part1} exercise={exercises1} />
+   <Part part={part2} exercise={exercises2} />
+   <Part part={part3} exercise={exercises3} />
+  </div>
+  )
+}
+
+const Part = ({part, exercise}) => {
+  return (
+    <p>{part} {exercise}</p>
   )
 }
 
 const Total = ({total}) => {
   return (
-  <>
    <p>Number of exercises {total}</p>
-  </>
   )
 }
 
