87dc94ce68e183edf21f19e37118ed9fd6f8ebf6
aRRRtsi <arrrtsi@gmail.com>
2022-09-15 18:23:21 +0300

    1.3: kurssitiedot, step3
    Muutin vakiot olioiksi kuten ohjeistuksessa, jätin vielä vanhat vakiot muistiin

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,11 +1,29 @@
 const App = () => {
   const course = 'Half Stack application development'
+  
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
+
+  const part1obj = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+
   const part2 = 'Using props to pass data'
   const exercises2 = 7
+
+  const part2obj = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+
   const part3 = 'State of a component'
   const exercises3 = 14
+  
+  const part3obj = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
@@ -18,9 +36,7 @@ const App = () => {
 }
 
 const Header = ({course}) => {
-  return (
-   <h1>{course}</h1>
-  )
+  return <h1>{course}</h1>
 }
 
 const Content = ({part1, part2, part3, exercises1, exercises2, exercises3}) => {
